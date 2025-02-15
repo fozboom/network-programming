182cacdee093c8d540f387b0903dcd976963cf94
d.hniatsetski <d.hniatsetski@meg-analytics.com>
2025-02-15 19:06:27 +0300

    implement basic logic: works

--- Server.py
+++ Server.py
@@ -2,12 +2,13 @@ import socket
 import threading
 import datetime
 import os
-from rich.console import Console
-from rich.progress import Progress
+from rich.progress import Progress, BarColumn, TimeElapsedColumn, TransferSpeedColumn, Console
 import time
-import Client
+
+
 from typing import Dict, Tuple
 console = Console()
+
 
 class TCPServer:
     def __init__(self, host="0.0.0.0", port=12346):
@@ -17,7 +18,7 @@ class TCPServer:
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
-        self.incomplete_transfers = {}
+        self.interrupted_downloads: Dict[str, str | int] = dict()
 
 
     def start(self):
@@ -29,12 +30,8 @@ class TCPServer:
         while True:
             client_socket, addr = self.server_socket.accept()
             console.log(f"[cyan]Новое подключение от {addr}[/cyan]")
-
-            if addr[0] in self.incomplete_transfers:
-                console.log(f"[yellow]Продолжение незавершенной передачи от {addr}[/yellow]")
-                self.handle_incomplete_transfer(client_socket, addr)
-            else:
-                client_socket.sendall(b"HELLO\n")
+  
+            # client_socket.sendall(b"HELLO\n")
             thread = threading.Thread(target=self.handle_client, args=(client_socket, addr))
             thread.start()
 
@@ -63,13 +60,10 @@ class TCPServer:
 
                     if response:
                         client_socket.sendall(response.encode())
-                except socket.error:
-                    console.log(f"[red]Разрыв соединения {addr}[/red]")
-                    self.failed_users[addr[0]] = self.failed_users.get(addr[0], 0) + 1
                 except Exception as e:
                     console.log(f"[red]Ошибка с {addr}: {e}[/red]")
                     break
-
+                    
         console.log(f"[magenta]Отключение {addr}[/magenta]")
 
     def process_command(self, command, client_socket):
@@ -95,6 +89,8 @@ class TCPServer:
         else:
             return "Неизвестная команда\n"
 
+
+
     def upload_file(self, client_socket, filename):
         """Принимает файл от клиента и сохраняет его с прогресс-баром."""
         if not filename:
@@ -105,7 +101,12 @@ class TCPServer:
         client_socket.sendall(b"OK\n")  # Подтверждаем получение размера
 
         start_time = time.time()
-        with open(filename, "wb") as f, Progress() as progress:
+        with open(filename, "wb") as f, Progress(
+                                                "[blue]{task.description}",
+                                                 BarColumn(),
+        TimeElapsedColumn(),  # Показывает прошедшее время вместо ETA
+        TransferSpeedColumn(),  # Показывает скорость передачи:
+        ) as progress:
             task = progress.add_task(f"[green]Загрузка {filename}...", total=filesize)
 
             received = 0
@@ -117,8 +118,8 @@ class TCPServer:
                 received += len(chunk)
                 progress.update(task, advance=len(chunk))
         elapsed_time = time.time() - start_time
-        bitrate = (filesize*8) / elapsed_time / 1024
-        console.log(f"[bold blue]Скорость передачи: {bitrate:.2f} Kbps[/bold blue]")
+        bitrate = filesize / elapsed_time / (1024 * 1024)
+        console.log(f"[bold blue]Скорость передачи: {bitrate:.2f} MB/s[/bold blue]")
 
         console.log(f"[bold green]Файл {filename} загружен ({filesize} байт)[/bold green]")
 
@@ -126,29 +127,57 @@ class TCPServer:
         """Отправляет файл клиенту с прогресс-баром."""
         if not filename or not os.path.exists(filename):
             return "Файл не найден\n"
-
+        
         filesize = os.path.getsize(filename)
-        client_socket.sendall(b"READY\n")
-        client_socket.sendall(str(filesize).encode())  # Отправляем размер файла
-        client_socket.recv(1024)  # Ждем подтверждения
+        starts_from = 0
+        bytes_to_send = filesize
+        
+        client_ip = client_socket.getpeername()[0]
+        if client_ip== self.interrupted_downloads.get("client_ip", "") and filename == self.interrupted_downloads.get("filename", ""):
+            starts_from = self.interrupted_downloads['position'] + 1
+            bytes_to_send = filesize - starts_from
+            client_socket.sendall(f"RESUME {starts_from}".encode())
+            if client_socket.recv(1024).decode() == "NOT FOUND":
+                starts_from = 0
+                bytes_to_send = filesize
+            console.log(f"[yellow]Продолжение загрузки файла {filename} с позиции {starts_from}[/yellow]")
+        
+        client_socket.sendall(f"READY {bytes_to_send}".encode())
+        console.log(f"[bold blue]Отправка файла {filename} размером {filesize} байт[/bold blue]")
+        console.log(f"[bold blue]Наичинаем отправку с позиции {starts_from}[/bold blue]")
+      
+        _ = client_socket.recv(1024)  # OK                                                                                                                                                                                                                                                      
 
 
         start_time = time.time()
         try:
-            with open(filename, "rb") as f, Progress() as progress:
+            with open(filename, "rb") as f, Progress(
+                                                "[blue]{task.description}",
+                                                 BarColumn(),
+        TimeElapsedColumn(),  # Показывает прошедшее время вместо ETA
+        TransferSpeedColumn(),  # Показывает скорость передачи
+        "[bold blue]{task.percentage:.0f}%[/bold blue]"
+    ) as progress:
                 task = progress.add_task(f"[blue]Отправка {filename}...", total=filesize)
+                progress.update(task, completed=starts_from)
 
+                f.seek(starts_from)
 
-                while chunk := f.read(1024):
+                sent_bytes = 0
+                while (chunk := f.read(1024)):
                     client_socket.sendall(chunk)
+                    sent_bytes += len(chunk)
                     progress.update(task, advance=len(chunk))
-        except socket.error:
-            a = client_socket.getsockname()
-            console.log(f"[red]Ошибка соединения {a}[/red]")
+        except socket.error as e:
+            console.log(f"[red]Ошибка соединения {e}[/red]")
+            self.interrupted_downloads["client_ip"] = client_ip
+            self.interrupted_downloads["filename"] = filename
+            self.interrupted_downloads["position"] = sent_bytes
+
 
         elapsed_time = time.time() - start_time
-        bitrate = (filesize * 8)/ elapsed_time / 1024
-        console.log(f"[bold blue]Скорость передачи: {bitrate:.2f} Kbps[/bold blue]")
+        bitrate = filesize/ elapsed_time / (1024 * 1024)
+        console.log(f"[bold blue]Скорость передачи: {bitrate:.2f} MB/s[/bold blue]")
 
         console.log(f"[bold blue]Файл {filename} отправлен ({filesize} байт)[/bold blue]")
 
