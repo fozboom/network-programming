ac7c840338f403d4e095a4047937a36386fff715
fozboom <fozboom@gmail.com>
2025-03-26 23:27:29 +0300

    remove unused main

--- Server/main.py
+++
@@ -1,8 +0,0 @@
-import Server.tcp_server.TCPServer as TCPServer
-import Server.udp_server.server as server
-
-if __name__ == "__main__":
-    tcp_server = TCPServer.TCPServer()
-    udp_server = server.UDPServer()
-    tcp_server.start()
-    udp_server.start()
